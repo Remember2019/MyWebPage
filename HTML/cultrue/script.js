84c658c84a131d192c516bc7a04c37df14dbc049
Remember2019 <2481602093@qq.com>
2023-11-09 11:21:37 +0800

    delete the main path file

--- HTML/cultrue/script.js
+++
@@ -1,6 +0,0 @@
-function updateMenuPosition() {
-    var menu = document.querySelector('.menu');
-    menu.style.top = window.scrollY + 'px';
-}
-
-window.addEventListener('scroll', updateMenuPosition);
