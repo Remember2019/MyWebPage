fc8c691b40aa9174b011d259185647b393fe75e6
Remember2019 <2481602093@qq.com>
2023-11-09 15:47:20 +0800

    add some icons

--- HTML/cultrue/script.js
+++ HTML/cultrue/script.js
@@ -1,6 +1,3 @@
-function updateMenuPosition() {
-    var menu = document.querySelector('.menu');
-    menu.style.top = window.scrollY + 'px';
+function showLanguageMenu() {
+    alert('选择语言');
 }
-
-window.addEventListener('scroll', updateMenuPosition);
