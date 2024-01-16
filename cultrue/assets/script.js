146ef579aeace1c83fa893d7d922da91b403847d
Remember2019 <2481602093@qq.com>
2024-01-17 02:52:14 +0800

    add many element-plus content & add the darkmode

--- cultrue/assets/script.js
+++ cultrue/assets/script.js
@@ -1,4 +1,4 @@
-const data = {
+/*const data = {
     "messages":[],
     "stream":false,
     "model":"gpt-3.5-turbo-16k",
@@ -6,6 +6,16 @@ const data = {
     "presence_penalty":0,
     "frequency_penalty":0,
     "top_p":1
+}*/
+
+const data = {
+    "model":"gpt-4",
+    "stream":false,
+    "frequency_penalty":0,
+    "presence_penalty":0,
+    "temperature":0.6,
+    "top_p":1,
+    "messages":[],
 }
 
 function getRes() {
@@ -26,21 +36,16 @@ function hideSidebar() {
     document.getElementById('sidebar').className = 'menu-nav'
 }
 
-function showChatUI() {
-    let window_bg = document.getElementsByClassName("chat-bg")[0]
-    window_bg.style.display = "block";
-}
-
-function dismissChatUI() {
-    let window_bg = document.getElementsByClassName("chat-bg")[0]
-    window_bg.style.display = "none";
-}
-
 function sendPostRequest(data, callback) {
-    fetch("https://chatai.dingsm.top/api/openai/v1/chat/completions", {
+    fetch("https://ib-private-git-allstarproductions-gpt-50365e-allstarproductions.vercel.app/api/openai/v1/chat/completions", {
+        /*"https://chat6.dingsm.top/api/openai/v1/chat/completions"*/
         method: "POST",
         headers: {
-            "Authorization": "Bearer nk-ABC123456"
+            /*"Authorization": "Bearer nk-ABC123456",
+            "X-Openai-End-Point": "https://api.chatnio.net/v1",
+            "X-Openai-Api-Key": "sk-af53adfeee6ebf5ce10a5902c8216d6d79e3ce727dac4f5b9ec6fc0d20c20af4",
+            "Cookie": "LOBE_LOCALE=zh-CN; LOBE_THEME_PRIMARY_COLOR=undefined; LOBE_THEME_NEUTRAL_COLOR=undefined",*/
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         },
         body: JSON.stringify(data)
     })
@@ -55,6 +60,7 @@ function sendPostRequest(data, callback) {
         })
         .catch(function (error) {
             console.log(error);
+            errorTip('请检查网络连接')
         });
 }
 
@@ -99,3 +105,16 @@ function submit(){
     sendPostRequest(data, handleResponse);
     inputElement.value = '';
 }
+
+let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+let handleDarkModeChange = (e) => {
+    if (e.matches) {
+        // 系统处于暗黑模式，给 html 添加 "dark" 类
+        document.documentElement.classList.add('dark');
+    } else {
+        // 系统不处于暗黑模式，移除 html 的 "dark" 类
+        document.documentElement.classList.remove('dark');
+    }
+};
+darkModeMediaQuery.addListener(handleDarkModeChange);
+handleDarkModeChange(darkModeMediaQuery);
